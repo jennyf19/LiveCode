5a4bb59972aa27a155c620cd4aba61bb930db33f
jennyf19 <ferriesj@gmail.com>
2017-01-23 20:14:21 -0800

    finished live loops

--- 04LiveLoops/Program.cs
+++ 04LiveLoops/Program.cs
@@ -49,7 +49,27 @@ namespace _04LiveLoops
                         break;
                 }
             }
+
+            //reference variable called list, which points to an array, which contains 3 elements, 10, 11, 12
+            int[] list = new int[] { 10, 11, 12 , 15, -3, 10};
+            list[0] = 3; //changes '10' to a 3
+            list[list.Length - 3] = 4; //change '12' to 4
+            Console.WriteLine(list[2]);
+
+            processMyList(list);
+
             Console.ReadLine();
+        }
+
+        private static void processMyList(int[] listp)
+        {
+            listp[0] = 123;
+            foreach (int element in listp)
+            {
+                Console.Write(element + " ");
+            }
+            //original list is unaffected, but pointer to the reference is gone w/null
+            listp = null;
         }
     }
 }
