0588ca9664c105cf8f1aaff95e7ae7da107da2f4
jennyf19 <ferriesj@gmail.com>
2017-01-26 19:19:26 -0800

    Lecture 04 & 05

---
+++ 05LiveCodeStatic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05LiveCodeStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car pinto = new Car();
+            Console.WriteLine($"pinto is {pinto.SerialNumber}");
+
+            Car maverick = new Car();
+            Console.WriteLine($"maverick is {maverick.SerialNumber}");
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        private int _nextSerialNumber = 1;
+        //field that tells serial #
+        private int _serialNumber;
+
+        //constructor
+        public Car()
+        {
+            _serialNumber = _nextSerialNumber++;
+        }
+
+        public int SerialNumber
+        {
+            get { return _serialNumber; }
+        }
+    }
+}
---
+++ 05LiveEnum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05LiveEnum
+{
+    class Program
+    {
+        enum color { red, green, blue};
+        static void Main(string[] args)
+        {
+            //IsDefined is testing to see if the typeof(color) and the string match
+            Console.WriteLine(Enum.IsDefined(typeof(color), "red"));
+            Console.WriteLine(Enum.IsDefined(typeof(color), "11123"));
+            Console.WriteLine(Enum.IsDefined(typeof(color), "Green"));
+            Console.ReadLine();
+        }
+    }
+}
